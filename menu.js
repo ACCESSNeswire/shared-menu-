bf138d1d422f86e6c1f4034194b5727b032f190c
ACCESS Newswire <andrei.ivanoff@issuerdirect.com>
2026-04-16 22:28:07 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -416,7 +416,7 @@
             Login <i class="fas fa-user-circle"></i>
           </a>
           <a href="https://checkout.pressrelease.com/checkout/" class="press-btn">
-            Purchase Now <i class="fas fa-sign-in-alt"></i>
+            Purchase Now<i class="fas fa-sign-in-alt"></i>
           </a>
           <a href="https://checkout.pressrelease.com/checkout/cart" class="cart-btn" aria-label="Cart" title="Cart">
             <i class="fas fa-shopping-cart"></i>
