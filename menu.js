17a7dae586d83be70c9ce17b1e2c16a23527abda
ACCESS Newswire <andrei.ivanoff@issuerdirect.com>
2026-06-08 10:38:49 -0500

    Update menu.js
    added /cart

--- menu.js
+++ menu.js
@@ -652,7 +652,7 @@
             <a href="https://app.accessnewswire.com/login/pressrelease" target="_blank" class="login-btn">
               Login <i class="fas fa-user-circle"></i>
             </a>
-            <a href="https://checkout.pressrelease.com/checkout" class="press-btn">
+            <a href="https://checkout.pressrelease.com/checkout/cart" class="press-btn">
               Purchase Now <i class="fas fa-sign-in-alt"></i>
             </a>
           </div>
@@ -661,7 +661,7 @@
           <a href="https://app.accessnewswire.com/login/pressrelease" target="_blank" class="login-btn">
             Login <i class="fas fa-user-circle"></i>
           </a>
-          <a href="https://checkout.pressrelease.com/checkout" class="press-btn">
+          <a href="https://checkout.pressrelease.com/checkout/cart" class="press-btn">
             Purchase Now<i class="fas fa-sign-in-alt"></i>
           </a>
           <a href="https://checkout.pressrelease.com/checkout/cart" class="cart-btn" aria-label="Cart" title="Cart">
